e7e0637b6c6f05ab48882d408033419935725a9d
oolst71 <115809707+oolst71@users.noreply.github.com>
2022-10-14 21:12:12 +0200

    zfcdsrfcx
    xesrfrfgfxred

---
+++ Game/ClimbYggdrasil/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private BoxCollider2D col;
+    private float dirX = 0f;
+    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float jumpPower = 5f;
+    [SerializeField] private LayerMask jumpGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<BoxCollider2D>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dirX = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            rb.velocity = new Vector2(dirX * moveSpeed, jumpPower);
+        }
+
+
+    }
+    private bool IsGrounded()
+        {
+            return Physics2D.BoxCast(col.bounds.center, col.bounds.size, 0f, Vector2.down, .1f, jumpGround);
+        }
+}
