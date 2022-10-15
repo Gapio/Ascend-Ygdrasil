aaa910f4aea1d5f0510167f22cb7e2b8742f4338
Gapio <goksen.pakkan.6953@student.uu.se>
2022-10-15 11:22:19 +0200

    created a melee attack
    Might need to work on directions but we need sprites before that

--- Game/ClimbYggdrasil/Assets/Scripts/ShootFireball.cs
+++ Game/ClimbYggdrasil/Assets/Scripts/ShootFireball.cs
@@ -13,15 +13,38 @@ public class ShootFireball : MonoBehaviour
 
     void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetAxisRaw("Horizontal") != 0)
         {
             dirX = Input.GetAxisRaw("Horizontal");
-            dirY = Input.GetAxisRaw("Vertical");
-            GameObject fball = Instantiate(projectile, transform.position, Quaternion.identity);
-            fball.GetComponent<Rigidbody2D>().velocity = new Vector2(dirX * projSpeed, dirY * projSpeed);
-            fball.GetComponent<FireballBehavior>().damage = damage;
-        }  
-    }
+        }
 
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            GameObject fball = Instantiate(projectile, transform.position, Quaternion.identity);
+            if (!MovementLock())
+            {
+                fball.GetComponent<Rigidbody2D>().velocity = new Vector2(dirX * projSpeed, 0);
+                fball.GetComponent<FireballBehavior>().damage = damage;
+            }
+            else
+            {
+                dirX = Input.GetAxisRaw("Horizontal");
+                dirY = Input.GetAxisRaw("Vertical");
+                fball.GetComponent<Rigidbody2D>().velocity = new Vector2(dirX * projSpeed, dirY * projSpeed);
+                fball.GetComponent<FireballBehavior>().damage = damage;
+            }
+        }
+    }
+    public bool MovementLock()
+    {
+        if (Input.GetKey(KeyCode.U))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
 }
